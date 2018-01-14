48970cfef44440298e8487b7f8cd24c6ad98fb43
Luis Philipe Fidelis <luizsale10@gmail.com>
2018-01-14 15:14:47 -0200

    add songs and playlists

--- contracts/Doppler.sol
+++ contracts/Doppler.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.4.18;
 
 contract Doppler {
   
+  address public owner;
+  mapping(address=>mapping(bytes => bytes)) songs;
+  mapping(address=>mapping(bytes => mapping(uint => bytes))) playlists;
+
+  function Dopler() {
+    owner = msg.sender;
+  }
+
+  function addSong(bytes songName, bytes contentHash) public {
+    songs[msg.sender][songName] = contentHash;
+  }
+
+  function addPlaylist(bytes playlistName, bytes[] songNames) public {
+    for (uint i = 0; i < songNames.length; i++) {
+        playlists[msg.sender][playlistName][i] = songNames[i];
+    }
+  }
+
 }
