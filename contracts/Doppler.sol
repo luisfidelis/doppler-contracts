d879508b9bde0a185e0a4b70a0dd394e0861efa2
Luis Philipe Fidelis <luizsale10@gmail.com>
2018-01-14 15:32:58 -0200

    Song struct

--- contracts/Doppler.sol
+++ contracts/Doppler.sol
@@ -3,20 +3,36 @@ pragma solidity ^0.4.18;
 contract Doppler {
   
   address public owner;
-  mapping(address=>mapping(bytes => bytes)) songs;
-  mapping(address=>mapping(bytes => mapping(uint => bytes))) playlists;
+
+  mapping(address => Song[]) songs;
+  mapping(address=> mapping(bytes => Song[])) playlists;
+
+  event Songs(address owner, bytes songName, bytes content);
+  event Playlists(address owner, bytes playlistName, bytes[] songs);
+  
+  struct Song {
+    bytes songName;
+    bytes content;
+  }
 
   function Dopler() {
     owner = msg.sender;
   }
 
   function addSong(bytes songName, bytes contentHash) public {
-    songs[msg.sender][songName] = contentHash;
+    Song memory song = Song(songName,contentHash);
+    song.songName = songName;
+    song.content = contentHash;
+    Song[] storage userSongs = songs[msg.sender];
+    userSongs.push(song);
+    songs[msg.sender] = userSongs;
   }
 
-  function addPlaylist(bytes playlistName, bytes[] songNames) public {
-    for (uint i = 0; i < songNames.length; i++) {
-        playlists[msg.sender][playlistName][i] = songNames[i];
+  function addPlaylist(bytes playlistName, uint[] songIndexes) public {
+    Song[] memory userSongs = songs[msg.sender];
+    for (uint i = 0; i < songIndexes.length; i++) {
+        Song memory playlistSong = userSongs[songIndexes[i]];
+        playlists[msg.sender][playlistName].push(playlistSong);
     }
   }
 
